140028b224cc8b187ffd7e0038307d8199a1386f
juan.ahmed <juan.ahmed@divcom.ch>
2024-06-02 14:18:46 +0200

    Finition étape 3

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -3,9 +3,78 @@
 </script>
 
 <template>
-<h1>Accueil</h1>
+  <main class="page-accueil">
+    <h1>Découvrez nos sabres <strong>LASER</strong></h1>
+
+    <div class="sabres">
+      <div class="sabre">
+        <RouterLink to="/sabre/:id">
+          <figure>
+            <img src="/img/sl-cal.jpg" alt="Sabre laser Cal Kestis">
+            <figcaption>
+              <h2>Sabre laser Cal Kestis</h2>
+              <div class="prix">349 CHF</div>
+            </figcaption>
+          </figure>
+        </RouterLink>
+      </div>
+      <div class="sabre rupture">
+        <a href="sabre.html">
+          <figure>
+            <img src="/img/sl-cal.jpg" alt="Sabre laser Cal Kestis">
+            <figcaption>
+              <p>Noooon, les stocks sont vides !</p>
+              <h2>Sabre laser Cal Kestis</h2>
+              <div class="prix">349 CHF</div>
+            </figcaption>
+          </figure>
+        </a>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped>
+
+/********** STYLE PAGE ACCUEIL *********/
+.page-accueil .sabres {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.25rem;
+  align-content: space-evenly;
+  justify-content: center;
+  padding: 0;
+}
+
+.page-accueil .sabres .sabre {
+  overflow: hidden;
+  flex: 2 0 300px;
+  padding: 0;
+  border-radius: 10px;
+  box-shadow: 0 0.03rem 0.06rem rgba(0, 0, 0, 0.2), 0 0.1rem 0.3rem rgba(0, 0, 0, 0.2);
+  margin-bottom: 2rem;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+  background-color: #e72854;
+}
+
+.page-accueil .sabres .sabre:hover {
+  box-shadow: 0 0.03rem 0.06rem rgba(0, 0, 0, 0.6), 0 0.1rem 0.3rem rgba(0, 0, 0, 0.6);
+  background-color: #6bbbda;
+}
+
+.page-accueil .sabres .sabre.rupture {
+  background-color: #27282a;
+}
+
+.page-accueil .sabres .sabre figure {
+  padding: 0;
+  margin: 0;
+}
+
+.page-accueil .sabres .sabre figcaption {
+  padding: 1rem;
+  color: white;
+}
 
 </style>
--- src/views/FicheSabreView.vue
+++ src/views/FicheSabreView.vue
@@ -3,9 +3,53 @@
 </script>
 
 <template>
-<h1>Fiche sabre</h1>
+  <main class="page-sabre">
+    <div class="sabre">
+      <h1>Sabre laser Cal Kestis</h1>
+
+      <img src="/img/sl-cal.jpg" alt="Sabre laser Cal Kestis">
+
+      <p>
+        Le SL-Cal est un sabre laser de la gamme REPLIKA.
+        Inspiré par le sabre de Cal Kestis, dans le jeu
+        « Fallen Order » (réplique non officielle).
+      </p>
+
+      <ul class="details">
+        <li>Prix: 349 CHF</li>
+        <li>Stock: plus de 10 pièces</li>
+        <li>Longueur: 38cm</li>
+        <li>Poids: 0,600 kg</li>
+      </ul>
+    </div>
+  </main>
 </template>
 
 <style scoped>
+
+/********** STYLE PAGE SABRE *********/
+.page-sabre .sabre {
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+.page-sabre .sabre img {
+  border: 6px solid #e72854;
+}
+
+.page-sabre ul.details {
+  list-style-type: none;
+  padding: 0;
+  margin: 1rem 0;
+  font-size: 1.1rem;
+}
+
+.page-sabre ul.details li {
+  padding: .5em 1em;
+}
+
+.page-sabre ul.details li:nth-child(odd) {
+  background-color: #eeeeee;
+}
 
 </style>
